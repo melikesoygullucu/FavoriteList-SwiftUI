71902e501373ea19cc30d28a73f5c682e7d73346
melikesoygullucu <melikesoygul@hotmail.com>
2024-07-29 11:19:36 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Melike Soygüllücü on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
